e1330bf6fb784eb53bb5c10ddb5a77d9a505f2ce
Dinitha Santhupa <59077328+DinithaSanthupa@users.noreply.github.com>
2022-07-19 15:00:23 +0530

    Home page loading error fixed

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -5,3 +5,5 @@ export const Exercises = () => {
     <div>Exercises</div>
   )
 }
+
+export default Exercises
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { useState } from 'react';
 import { Box } from '@mui/system';
 
+import Exercises from '../components/Exercises';
+import SeacrhExercises from '../components/SeacrhExercises';
+import HeroBanner from '../components/HeroBanner';
+
 
 const Home = () => {
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises/>
+      <SeacrhExercises/>
       <Exercises/>
     </Box>
   )
